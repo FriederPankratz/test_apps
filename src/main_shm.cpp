417da5b33e80d2f48c92049eaac911150e2e6f94
Frieder Pankratz <frieder.pankratz@gmail.com>
2022-06-21 15:30:40 +0200

    - first working shm version

--- src/main_shm.cpp
+++ src/main_shm.cpp
@@ -256,14 +256,15 @@ void addDebugView(traact::DefaultInstanceGraphPtr &graph, int index) {
 
 }
 
-void addBasicProcessing(traact::DefaultInstanceGraphPtr &graph, int index, std::string filename) {
+void addBasicProcessing(traact::DefaultInstanceGraphPtr &graph, int index, const pattern::instance::PortGroupInstance& ir_group) {
     auto get_name = [index](const std::string &name) {
         return getIdxName(name, index);
     };
 
-    auto source_pattern =
-        graph->addPattern(get_name("source"),
-                          my_facade.instantiatePattern("traact::component::kinect::KinectAzureSingleFilePlayer"));
+
+    auto calibration_pattern =
+        graph->addPattern(get_name("calibration"),
+                          my_facade.instantiatePattern("artekmed::ShmSensorCalibrationSource"));
 
     auto convert_to_gray_pattern =
         graph->addPattern(get_name("convert_to_gray"), my_facade.instantiatePattern("OpenCvConvertImage"));
@@ -278,8 +279,10 @@ void addBasicProcessing(traact::DefaultInstanceGraphPtr &graph, int index, std::
 
 
     // configure
-    source_pattern->setParameter("file", filename);
-    source_pattern->setParameter("stop_after_n_frames", -1);
+
+    calibration_pattern->setParameter("stream", fmt::format("camera{0:02d}", index+1));
+
+
 
     int gray_threshold = 80;
     int max_blob_size = 24;
@@ -310,13 +313,15 @@ void addBasicProcessing(traact::DefaultInstanceGraphPtr &graph, int index, std::
 //    refine_circles_pattern->setParameter("MaxRadius", max_blob_size/2);
 
     // setup connections
-    graph->connect(get_name("source"), "output_ir", get_name("convert_to_gray"), "input");
+    graph->connect("image_source", ir_group.getProducerPortName("output"), get_name("convert_to_gray"), "input");
+
+
 
     graph->connect(get_name("convert_to_gray"), "output", get_name("undistort"), "input");
-    graph->connect(get_name("source"), "output_ir_calibration", get_name("undistort"), "input_calibration");
+    graph->connect(get_name("calibration"), "output_ir_calibration", get_name("undistort"), "input_calibration");
     graph->connect(get_name("undistort"), "output", get_name("blob_detection"), "input");
-    //graph->connect(getName("undistort"), "output", getName("refine_circles"), "input");
-    //graph->connect(getName("blob_detection"), "output", getName("refine_circles"), "input_points");
+    //graph->connect(get_name("undistort"), "output", get_name("refine_circles"), "input");
+    //graph->connect(get_name("blob_detection"), "output", get_name("refine_circles"), "input_points");
 }
 
 int main(int argc, char **argv) {
@@ -327,11 +332,11 @@ int main(int argc, char **argv) {
 
     signal(SIGINT, ctrlC);
 
-    util::initLogging(spdlog::level::debug);
+    util::initLogging(spdlog::level::trace);
 
     DefaultInstanceGraphPtr graph = std::make_shared<DefaultInstanceGraph>("tracking");
 
-    int camera_count = 6;
+    int camera_count = 5;
     std::string video_pattern = "/home/frieder/data/recording_20210611_calib1/cn{0:02d}/k4a_capture.mkv";
     //std::string init_file_pattern = "/home/frieder/data/recording_20210611_calib1/cn{0:02d}/new_camera2world.json";
     std::string init_file_pattern = "/home/frieder/data/recording_20210611_calib1/cn{0:02d}/camera2world_final.json";
@@ -339,23 +344,27 @@ int main(int argc, char **argv) {
     std::string result_file_pattern =           "/home/frieder/data/recording_20210611_calib1/data2/{0}.json";
     std::string result_camera_file_pattern =    "/home/frieder/data/recording_20210611_calib1/data2/{0}_{1:02d}.json";
 
-    for (int camera_index = 0; camera_index < camera_count; ++camera_index) {
-        auto video_file =
-            fmt::format(video_pattern, camera_index + 1);
-        addBasicProcessing(graph, camera_index, video_file);
-        addDebugView(graph, camera_index);
+    auto source_pattern =
+        graph->addPattern("image_source",
+                          my_facade.instantiatePattern("artekmed::ShmCompositeBufferSource"));
+    source_pattern->setParameter("stream", "camera_x5_shm");
 
+    for (int camera_index = 0; camera_index < camera_count; ++camera_index) {
+        auto& ir_group = source_pattern->instantiatePortGroup("ir_image");
+        ir_group.setParameter("channel", fmt::format("camera{0:02d}_infraredimage", camera_index+1));
+        addBasicProcessing(graph, camera_index, ir_group);
+        addDebugView(graph, camera_index);
     }
     //addInitTracking(graph, camera_count, init_file_pattern);
     addPointEstimation(graph, camera_count, init_file_pattern);
     addOutsideInTargetTracking(graph, camera_count);
-    addOutsideInTargetRecord(graph, camera_count, result_file_pattern, result_camera_file_pattern);
+    //addOutsideInTargetRecord(graph, camera_count, result_file_pattern, result_camera_file_pattern);
 
     buffer::TimeDomainManagerConfig td_config;
     td_config.time_domain = 0;
     td_config.ringbuffer_size = 3;
     td_config.master_source = "source";
-    td_config.source_mode = SourceMode::WAIT_FOR_BUFFER;
+    td_config.source_mode = SourceMode::IMMEDIATE_RETURN;
     td_config.missing_source_event_mode = MissingSourceEventMode::WAIT_FOR_EVENT;
     td_config.max_offset = std::chrono::milliseconds(15);
     td_config.max_delay = std::chrono::milliseconds(100);
