30fbdfc013602b012b919587f6bb1508a4cfb64e
Frieder Pankratz <frieder.pankratz@gmail.com>
2023-09-05 23:07:27 +0200

    update to conan 2

--- conanfile.py
+++ conanfile.py
@@ -1,55 +1,48 @@
 # /usr/bin/python3
 import os
-from conans import ConanFile, CMake, tools
-
+from conan import ConanFile
+from conan.tools.build import can_run
 
 class TraactPackage(ConanFile):
-    name = "app_ir_tracking"
-    version = "1.0.0"
+    python_requires = "traact_base/0.0.0@traact/latest"
+    python_requires_extend = "traact_base.TraactPackageCmake"
 
+    name = "test_apps"
+    version = "0.0.0"
     description = ""
     url = ""
-    license = ""
-    author = ""
-
-    short_paths = True
-
-    generators = "cmake", "TraactVirtualRunEnvGenerator"
+    license = "MIT"
+    author = "Frieder Pankratz#"
+    
     settings = "os", "compiler", "build_type", "arch"
     compiler = "cppstd"
+
     options = {
-        "shared": [True, False],
-        "with_tests": [True, False]
+        "shared": [True, False]
     }
 
     default_options = {
-        "shared": True,
-        "with_tests": True
+        "shared": True
     }
 
     exports_sources = "CMakeLists.txt", "src/*"
 
-    # overwrite these dependencies
-    requires = (
-        "eigen/3.4.0",
-        "spdlog/1.11.0",
-        "glfw/3.3.4",
-        "opencv/4.5.5@camposs/stable"
-    )
-
     def requirements(self):
-        self.requires("traact_run_env/1.0.0@traact/latest")
-        self.requires("traact_core/1.0.0@traact/latest")
-        self.requires("traact_spatial/1.0.0@traact/latest")
-        self.requires("traact_vision/1.0.0@traact/latest")
-        self.requires("traact_component_basic/1.0.0@traact/latest")
-        self.requires("traact_component_kinect_azure/1.0.0@traact/latest")
-        self.requires("traact_component_cereal/1.0.0@traact/latest")
-        self.requires("traact_component_aruco/1.0.0@traact/latest")
-        self.requires("traact_component_pcpd_shm/1.0.0@traact/latest")
-        self.requires("traact_component_pointcloud/1.0.0@traact/latest")
-        self.requires("traact_component_http/1.0.0@traact/latest")
-        self.requires("yaml-cpp/0.6.3")
+        self.requires("traact_base/0.0.0@traact/latest")
+        self.requires("traact_core/0.0.0@traact/latest")
+        self.requires("traact_spatial/0.0.0@traact/latest")
+        self.requires("traact_vision/0.0.0@traact/latest")
+        self.requires("traact_gui/0.0.0@traact/latest")
+        self.requires("traact_component_basic/0.0.0@traact/latest")
+        self.requires("traact_component_kinect_azure/0.0.0@traact/latest")
+        self.requires("traact_component_cereal/0.0.0@traact/latest")
+        self.requires("traact_component_aruco/0.0.0@traact/latest")
+        self.requires("traact_component_pcpd_shm/0.0.0@traact/latest")
+        self.requires("traact_component_pointcloud/0.0.0@traact/latest")
+        self.requires("traact_component_http/0.0.0@traact/latest")
+        self.requires("yaml-cpp/0.7.0")
+
+        self.requires("cpp-httplib/0.14.0", transitive_libs=True)
 
     def configure(self):
         self.options['traact_core'].shared = self.options.shared
